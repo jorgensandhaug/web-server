3d215b3f9e347973e0feacedfd7f88f0e726636e
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-03-10 21:02:17 +0100

    la til routes for jorgen

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const server = require("http").Server(app)
 
-app.get("/jorgen", (req, res) =>{
+app.get("/jorge", (req, res) =>{
     res.sendFile("index.html", {root: "/home/sandhaug/jorge/nettside-jorgen/"});
 })
 
@@ -13,6 +13,38 @@ app.get("/simen", (req, res) =>{
 app.get("/", (req, res) =>{
     res.send("Home page");
 })
+
+
+const routes = [
+    ("jorge", "sound_visualizer", "nettside-jorgen/Prosjekter/SoundVisualizer"),
+    ("jorge", "tetris", "nettside-jorgen/Prosjekter/Tetris"),
+    ("jorge", "tank", "nettside-jorgen/Prosjekter/Tank, A Fullfilling Gaming Experience"),
+    ("jorge", "tegneprogram", "nettside-jorgen/Prosjekter/Drawing"),
+    ("jorge", "platformer", "nettside-jorgen/Prosjekter/Platformer")
+    ];
+
+routes.forEach( tup =>{
+    const site, name, path = tup;
+    app.get("/"+site+"/"+name+"/"+path, (req, res) =>{
+        res.sendFile("index.html", {root: "/home/sandhaug/"+site+"/"+path+"/"});
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const PORT = 80
 server.listen(PORT)
