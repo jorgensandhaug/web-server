2ee3d52e7059892f18838460f1d22310da0bb627
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-03-10 19:48:51 +0100

    removed jorgens nettside

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-const express = require("express")
-const app = express()
-const server = require("http").Server(app)
+// const express = require("express")
+// const app = express()
+// const server = require("http").Server(app)
 
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.static("public"))
+// app.use(express.urlencoded({ extended: true }))
 
-const PORT = 80
-server.listen(PORT)
+// const PORT = 80
+// server.listen(PORT)
+
+
+// Require diet
+var server = require('diet');
+
+// Main domain
+var app = server()
+app.listen('http://example.com/')
+app.get('/', function($){
+    $.end('hello world ')
+})
+
+// Sub domain
+var sub = server()
+sub.listen('http://subdomain.example.com/')
+sub.get('/', function($){
+    $.end('hello world at sub domain!')
+})
+
+// Other domain
+var other = server()
+other.listen('http://other.com/')
+other.get('/', function($){
+    $.end('hello world at other domain')
+})
