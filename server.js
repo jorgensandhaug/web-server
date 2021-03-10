372ac7150f4ace1cd88a68e76789cee08b0036d9
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-03-10 20:01:34 +0100

    fixed routing

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
-// const express = require("express")
-// const app = express()
-// const server = require("http").Server(app)
+const express = require("express")
+const app = express()
+const server = require("http").Server(app)
 
-// app.use(express.static("public"))
-// app.use(express.urlencoded({ extended: true }))
-
-// const PORT = 80
-// server.listen(PORT)
-
-
-// Require diet
-var server = require('diet');
-
-// Main domain
-var app = server()
-app.listen('http://example.com/')
-app.get('/', function($){
-    $.end('hello world ')
+app.get("/jorgen", (req, res) =>{
+    res.sendFile("~/jorge/nettside-jorgen/index.html");
 })
 
-// Sub domain
-var sub = server()
-sub.listen('http://subdomain.example.com/')
-sub.get('/', function($){
-    $.end('hello world at sub domain!')
+app.get("/simen", (req, res) =>{
+    res.sendFile("~/simen/nettside/index.html");
 })
 
-// Other domain
-var other = server()
-other.listen('http://other.com/')
-other.get('/', function($){
-    $.end('hello world at other domain')
-})
+const PORT = 80
+server.listen(PORT)
+
