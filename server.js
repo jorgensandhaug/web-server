f0445e308a69f2f1777a85bd9e3295380b0fa3be
jorgensandhaug <jorgen.sandhaug@gmail.com>
2021-05-05 20:07:22 +0200

    changed port to 8000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const server = require("http").Server(app)
 
-app.use(express.static("public"));
+app.use(express.static("/home/sandhaug/web-server/public"));
 
 
 const PORT = 8000
