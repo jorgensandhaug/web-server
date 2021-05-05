0c60fe34d2cb277a71433380d746188708617789
jorgensandhaug <jorgen.sandhaug@gmail.com>
2021-05-05 20:21:39 +0200

    added dynamic path for public folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const server = require("http").Server(app)
 
-app.use(express.static("/home/sandhaug/web-server/public"));
+app.use(express.static(__dirname+"/public"));
 
 
 const PORT = 8000
